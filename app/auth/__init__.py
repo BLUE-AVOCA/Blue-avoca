f1e20bb82b96e715fe154b50829e6cda1b7fe445
Hoàng Ngọc Dung <dunghoang0401237@gmail.com>
2023-10-15 07:18:30 +0700

    Revert "up"
    This reverts commit a97febe6a2fb3fdead32cf86d3d3fb43e6025870.

--- app/auth/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-import os
-
-bp = Blueprint('auth', __name__)
-
-from app.auth import routes
--- app/connected/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-from app.connected import bp
-from app.models.test2 import Company
-
-@bp.route('/')
-def index():
-    companies = Company.query.all()
-    return render_template('connected/base.html', companies = companies)
